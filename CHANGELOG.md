770ca9dc17480b6f0d0b09dda82a8e51aa3785a8
Liu Yue <hangxingliu@gmail.com>
2018-04-18 01:00:18 +0800

    1.3.2

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+### 1.3.2 (2018-04-18)
+
+1. support `all` config matcher for matching all config.
+2. fix bug about comments in `configutaions` field in config file.
+3. support double dash in arguments
--- src/build-options.cc
+++ src/build-options.cc
@@ -7,8 +7,8 @@
 #include "./build-options.hpp"
 
 const char* PROGRAM_NAME = "backup-helper";
-const char* VERSION = "1.2.2";
-const char* VERSION_DATE= "2018-02-27";
+const char* VERSION = "1.3.2";
+const char* VERSION_DATE= "2018-04-18";
 
 const std::vector<std::string> CONFIG_FILES = { // NOLINT
 		"backup-config.debug.json",
